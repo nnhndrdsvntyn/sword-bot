7152215a0c3c5be15ade6dbfcc9d23efe2a5f1ad
nnhndrdsvntyn <jarkiezzzzonmc@gmail.com>
2025-07-21 21:52:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // === CONFIG ===
-let BOT_Mode = "Free-XP"; // Options: 'Free-XP' or 'Find-Kill-NPCs-Players'
-let farmAdXP = true; // Toggle bonus XP farming
-let reconnect_aboveScore = false; // Reset bots if score > SPAWN_SCORE
+let BOT_Mode = "Find-Kill-NPCs-Players"; // Options: 'Free-XP' or 'Find-Kill-NPCs-Players'
+let farmAdXP = false; // Toggle bonus XP farming
+let reconnect_aboveScore = true; // Reset bots if score > SPAWN_SCORE
 
-const SPAWN_SCORE = 500_000;
-const servers = ["na-4"];
-const botsPerServer = 150;
+const SPAWN_SCORE = 10;
+const servers = ["na-3"];
+const botsPerServer = 75;
 
 // === EXPRESS SERVER ===
 const express = require("express");
