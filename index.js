1eef587d71f090c9818447bfa9a2b02f2f35eb83
nnhndrdsvntyn <jarkiezzzzonmc@gmail.com>
2025-08-03 21:12:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,21 @@
 const https = require("https");
 
-const SERVERS = [
-  "na",
-  "na-7",
-  "na-2",
-  "na-3",
-  "na-4",
-  "na-5",
-  "na-6",
-  "na-7",
-  "na-8",
-];
-const MAX_PER_SERVER = 10;
-const DELAY_MS = 1;
+const SERVERS = ["na", "na-7", "na-2", "na-3", "na-4", "na-5", "na-6", "na-8"];
+const connections = [];
 
-let serverIndex = 0;
-let count = 0;
-let connections = [];
-
-function sendPoll() {
-  if (count >= MAX_PER_SERVER) {
-    console.log(
-      `\nCompleted 180 requests to ${SERVERS[serverIndex]}\nSwitching to next server...\n`,
-    );
-    count = 0;
-    serverIndex = (serverIndex + 1) % SERVERS.length;
-    connections = [];
-  }
-
-  const server = SERVERS[serverIndex];
-  const url = `https://${server}.swordonline.io/socket.io/?EIO=3&transport=polling`;
-
-  const req = https.get(url, (res) => {
-    res.on("data", () => {});
-    res.on("end", () => {});
+function pollAll() {
+  SERVERS.forEach((server) => {
+    const url = `https://${server}.swordonline.io/socket.io/?EIO=3&transport=polling`;
+    const req = https.get(url, (res) => {
+      // Just consume data, but do NOT destroy socket or end connection
+      res.on("data", () => {});
+      res.on("end", () => {});
+    });
+    req.on("error", () => {});
+    connections.push(req); // Keep request object to keep connection alive
   });
 
-  req.on("error", () => {});
-
-  connections.push(req);
-
-  count++;
-  if (count % 10 === 0) {
-    console.log(`Sent ${count} requests to ${server}`);
-  }
-
-  setTimeout(sendPoll, DELAY_MS);
+  setImmediate(pollAll);
 }
 
-sendPoll();
-
+pollAll();
